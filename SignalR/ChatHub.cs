1f36cf5da8780bcac5f6078e454afdec449a39cd
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-08-24 00:31:51 +0400

    Add project files.

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string username,string message )
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected",$"{Context.ConnectionId}");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", $"{Context.ConnectionId}");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
